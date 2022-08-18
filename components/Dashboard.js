3ec9485cb311d37418917c2f25ea0300fbf8bcd5
manishchaurasiy <109471315+manishchaurasiy@users.noreply.github.com>
2022-08-18 16:19:17 +0530

    sjsjs

---
+++ components/Dashboard.js
@@ -0,0 +1,13 @@
+import Right from "./Right";
+import Body from "./Body";
+import Sidebar from "./Sidebar";
+
+function Dashboard() {
+  return <main>
+      <Sidebar />
+      <Body />
+      <Right />
+    </main>;
+  }
+
+export default Dashboard;
